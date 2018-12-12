b53dbaa50138cb02203206071461bd5c07f8c64d
Connor Kam-Cheong <connorkc42@gmail.com>
2018-12-12 16:31:03 +0000

    Refactored 'post names' to redirect and use session variables

--- app.rb
+++ app.rb
@@ -1,16 +1,24 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb :index
   end
 
   post '/names' do
-    erb :play, locals:{
-        player_1: params[:player_1],
-        player_2: params[:player_2]
-      }
+    session[:player_1] = params[:player_1]
+    session[:player_2] = params[:player_2]
+
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+  
+    erb :play
   end
 
   # start the server if ruby file executed directly
