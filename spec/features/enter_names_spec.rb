9ac2a3a4635958dbf7a9de41c421e887f0e02a80
Connor Kam-Cheong <connorkc42@gmail.com>
2018-12-15 15:59:29 +0000

    Attack page links back to play page

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -2,6 +2,6 @@ feature "Enter names" do
   scenario "submitting names" do
     sign_in_and_play()
 
-    expect(page).to have_content('A wild Pikachu and Charizard appeared!')
+    expect(page).to have_content('Pikachu vs. Charizard')
   end
 end
